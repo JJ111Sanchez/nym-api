ba1c057b165587a5737a65ac12b3380df8b79d48
noisk8 <juansk8jaramillo@gmail.com>
2023-12-31 03:07:00 -0500

    visualizador funciona por consola,ssss

--- src/app/visualizer/page.tsx
+++ src/app/visualizer/page.tsx
@@ -1,80 +1,23 @@
-'use client'
-import React, { useState, useEffect } from "react";
-import PocketBase from 'pocketbase'; 
+import React, { useEffect } from 'react';
 import axios from 'axios';
-import { createNymMixnetClient, NymMixnetClient } from '@nymproject/sdk-full-fat';
-
-const nymApiUrl = "https://validator.nymtech.net/api";
 
 const DataViewComponent = () => {
-  const [nym, setNym] = useState<NymMixnetClient>();
-  const [data, setData] = useState([]);
-
   useEffect(() => {
-    const init = async () => {
-      const client = await createNymMixnetClient();
-      setNym(client);
-
-      // Iniciar el cliente y conectar a un gateway
-      await client?.client.start({
-        clientId: crypto.randomUUID(),
-        nymApiUrl,
-        forceTls: true, // forzar WSS
-      });
-    };
-
     const fetchData = async () => {
       try {
         const result = await axios.get('https://cyberguenza.pockethost.io/api/collections/formulario/records');
-        setData(result.data);
-
-        // Enviar los datos a través de la mixnet de Nym
-        // Asegúrate de que nym no es nulo antes de intentar enviar datos
-        if (nym?.client) {
-          // Define el destinatario y el payload para el envío de datos
-          const sendConfig = {
-            payload: { message: JSON.stringify(result.data) },
-            recipient: 'recipient.address', // Reemplaza con la dirección del destinatario real
-            replySurbs: 0 // No esperar respuesta
-          };
-          await nym.client.send(sendConfig);
-        }
-
-        console.log('Datos recibidos:', result.data);
+        console.log(result.data);
       } catch (error) {
         console.error('Error al recuperar los datos:', error);
       }
     };
 
-    init().then(fetchData);
-
-    return () => {
-      nym?.client.stop();
-    };
+    fetchData();
   }, []);
 
   return (
-    <div className="container mx-auto">
-      <table className="table-auto w-full">
-        <thead>
-          <tr className="bg-gray-200">
-            <th className="px-4 py-2">ID</th>
-            <th className="px-4 py-2">Nombre</th>
-            <th className="px-4 py-2">Correo</th>
-            <th className="px-4 py-2">Mensaje</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((item, index) => (
-            <tr key={index} className="border-b">
-              <td className="px-4 py-2">{}</td>
-              <td className="px-4 py-2">{}</td>
-              <td className="px-4 py-2">{}</td>
-              <td className="px-4 py-2">{}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <div>
+      <p>Verifica la consola para ver los datos.</p>
     </div>
   );
 };
