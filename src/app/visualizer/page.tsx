2c1c5a56239f2dab4b75e80aca235f63f24fec7b
noisk8 <juansk8jaramillo@gmail.com>
2024-01-10 17:04:26 -0500

    datos en tabla con estilo

--- src/app/visualizer/page.tsx
+++ src/app/visualizer/page.tsx
@@ -60,24 +60,24 @@ const Visualizer = () => {
    
     <div className="flex flex-col">
 
-      <h1 className="text-white"> hola</h1>
-  <div className="overflow-x-auto sm:mx-0.5 lg:mx-0.5">
+      <h1 className=" text-center pb-4 pt-4 text-3xl text-teal-500"> Datos ingresados vía mixnet </h1>
+  <div className="overflow-x-auto sm:mx-0.5 lg:mx-0.5  shadow-lg shadow-teal-700">
     <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
       <div className="overflow-hidden">
         <table className="min-w-full">
-          <thead className="bg-white border-b">
+          <thead className="bg-teal-900 border-b">
             <tr>
-              <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+              <th scope="col" className="text-sm font-medium text-white px-6 py-4 text-center">
               collectionId
 
               </th>
-              <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+              <th scope="col" className="text-sm font-medium text-white px-6 py-4 text-center">
                 name
               </th>
-              <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+              <th scope="col" className="text-sm font-medium text-white px-6 py-4 text-center">
                 lastName
               </th>
-              <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+              <th scope="col" className="text-sm font-medium text-white px-6 py-4 text-center">
                 email
               </th>
             </tr>
@@ -85,11 +85,11 @@ const Visualizer = () => {
           <tbody>
             {/* Iterar sobre el estado de los datos y crear filas de tabla con los datos name y lastName */}
             {data.map(item => (
-              <tr className="bg-gray-100 border-b" key={item.id}>
-                <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{item.collectionId}</td>
-                <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{item.name}</td>
-                <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{item.lastName}</td>
-                <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{item.email}</td>
+              <tr className="bg-teal-400 border-b" key={item.id}>
+                <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 text-center">{item.collectionId}</td>
+                <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap text-center">{item.name}</td>
+                <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap text-center">{item.lastName}</td>
+                <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap text-center">{item.email}</td>
               </tr>
             ))}
           </tbody>
